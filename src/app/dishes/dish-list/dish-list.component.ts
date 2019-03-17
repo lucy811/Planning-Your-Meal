90096de4420302667ae071419338ca7cb63b2870
Jingting <jchen136@hawk.iit.edu>
2019-03-16 21:53:12 -0500

    Create dish component

--- src/app/dishes/dish-list/dish-list.component.ts
+++ src/app/dishes/dish-list/dish-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../dish.model';
 
 @Component({
   selector: 'app-dish-list',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DishListComponent implements OnInit {
 
+  dishes: Dish[] = [
+    new Dish('Gongbao Chicken', 'Chinese Food', 'http://farm7.static.flickr.com/6127/5949832181_d73a87f299_z.jpg'),
+    new Dish('Hot dry noodles', 'Great Noodles', 'http://www.globaltimes.cn/Portals/0/attachment/2011/5997c21f-a92a-4850-b5a9-bd0e598fd354.jpeg'),
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
