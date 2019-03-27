251e45d5fa899b9c2ba6bba8674d1033e0245f73
Jingting <jchen136@hawk.iit.edu>
2019-03-26 21:17:08 -0500

    Add ingredients to dish detail

--- src/app/dishes/dish.model.ts
+++ src/app/dishes/dish.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Dish {
   name: string;
   description: string;
   imagePath: string;
+  ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredient: Ingredient[]) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.ingredients = ingredient;
   }
 }
--- src/app/dishes/dish.service.ts
+++ src/app/dishes/dish.service.ts
@@ -1,5 +1,6 @@
 import { Dish } from '../dishes/dish.model';
 import { EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class DishService {
   @Output() dishSelected = new EventEmitter<Dish>();
@@ -7,10 +8,18 @@ export class DishService {
   dishes: Dish[] = [
     new Dish('Gongbao Chicken',
              'Chinese Food',
-             'http://farm7.static.flickr.com/6127/5949832181_d73a87f299_z.jpg'),
+             'http://farm7.static.flickr.com/6127/5949832181_d73a87f299_z.jpg',
+             [
+               new Ingredient('chicken', 1),
+               new Ingredient('cucumber', 2)
+             ]),
     new Dish('Hot dry noodles',
              'Great Noodles',
-             'http://www.globaltimes.cn/Portals/0/attachment/2011/5997c21f-a92a-4850-b5a9-bd0e598fd354.jpeg'),
+             'http://www.globaltimes.cn/Portals/0/attachment/2011/5997c21f-a92a-4850-b5a9-bd0e598fd354.jpeg',
+             [
+              new Ingredient('noodles', 2),
+              new Ingredient('hot pepper', 1)
+             ]),
   ];
 
   getDishes() {
