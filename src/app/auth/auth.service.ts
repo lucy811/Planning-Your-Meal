ca59f5cadc10858a3fdba90da790598156dc093e
Jingting <jchen136@hawk.iit.edu>
2019-04-09 15:56:36 -0500

    Add token and logout function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .catch(
@@ -11,9 +13,32 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(
-      response => console.log(response)
+      response => {
+        // get token when signin
+        firebase.auth().currentUser.getIdToken()
+        .then(
+          (token: string) => this.token = token
+        );
+      }
     ).catch(
       error => console.log(error)
     );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken()
+    .then(
+      (token: string) => this.token = token
+    );
+    return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
+  }
+
+  logout() {
+    firebase.auth().signOut();
+    this.token = null;
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { DishService } from '../dishes/dish.service';
+import { AuthService } from '../auth/auth.service';
 import { Dish } from '../dishes/dish.model';
 import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private dishService: DishService) {}
+  constructor(private http: Http, private dishService: DishService, private authService: AuthService) {}
 
   storeDishes() {
-    return this.http.put('https://meal-plan-application.firebaseio.com/dishes.json', this.dishService.getDishes());
+    const token = this.authService.getToken();
+    return this.http.put('https://meal-plan-application.firebaseio.com/dishes.json?auth=' + token, this.dishService.getDishes());
   }
 
   getDishes() {
-    this.http.get('https://meal-plan-application.firebaseio.com/dishes.json').map(
+    const token = this.authService.getToken();
+    this.http.get('https://meal-plan-application.firebaseio.com/dishes.json?auth=' + token).map(
       (response: Response) => {
         const dishes: Dish[] = response.json();
         for (const dish of dishes) {
