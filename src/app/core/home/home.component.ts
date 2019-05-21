1510c990dc9ee963d2eed65deb4b70edfed8b877
Jingting <jchen136@hawk.iit.edu>
2019-05-21 14:26:15 -0500

    Add animation in home component

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,10 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { trigger, style, transition, animate, keyframes } from '@angular/animations';
+  
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('home', [
+      transition('void => *', [
+        animate(1500, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 0.7,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+    ]),
+  ]
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
